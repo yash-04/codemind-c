6b748d9734b2622f21b66d4082705e57f6f6925d
yash-04 <82692651+yash-04@users.noreply.github.com>
2023-01-26 10:06:26 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,j,k=0,c=0,mx=0,mn=0,m=0;     int a;     scanf("%d",&a);     c=0;     k=0;     for(i=0;i<a;i++)     {         c=0;         for(j=2;j<a+k;j++)         {             if((a+k)%j==0)             {                 c++;             }         }         if(c==0)         {             mx=a+k;             break;         }         k++;     }     c=0;     m=0;     for(i=0;i<a;i++)     {         c=0;         for(j=2;j<a-m;j++)         {             if((a-m)%j==0)             {                 c++;             }         }         if(c==0)         {             mn=a-m;             break;         }         m++;     }     int min=a-mn,max=mx-a;     if(min>max)     printf("%d",max);     else     printf("%d",min); }

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,j,k=0,c=0,mx=0,mn=0,m=0;
+    int a;
+    scanf("%d",&a);
+    c=0;
+    k=0;
+    for(i=0;i<a;i++)
+    {
+        c=0;
+        for(j=2;j<a+k;j++)
+        {
+            if((a+k)%j==0)
+            {
+                c++;
+            }
+        }
+        if(c==0)
+        {
+            mx=a+k;
+            break;
+        }
+        k++;
+    }
+    c=0;
+    m=0;
+    for(i=0;i<a;i++)
+    {
+        c=0;
+        for(j=2;j<a-m;j++)
+        {
+            if((a-m)%j==0)
+            {
+                c++;
+            }
+        }
+        if(c==0)
+        {
+            mn=a-m;
+            break;
+        }
+        m++;
+    }
+    int min=a-mn,max=mx-a;
+    if(min>max)
+    printf("%d",max);
+    else
+    printf("%d",min);
+}
